d224120fa38fefcc158ed2617635a5e7ad84cd78
Kostas Kougios <kostas.kougios@googlemail.com>
2025-03-16 11:55:40 +0000

    -

--- easygame-model/src/main/scala/org/easygame/model/StateForFrame.scala
+++ easygame-model/src/main/scala/org/easygame/model/StateForFrame.scala
@@ -1,8 +1,10 @@
 package org.easygame.model
 
+import monocle.syntax.all.*
 import org.easygame.view.ViewPort
 
 case class StateForFrame[S](
     state: S,
     viewPorts: Seq[ViewPort] = Nil
-)
+):
+  def changeState(s: S) = this.focus(_.state).replace(s)
